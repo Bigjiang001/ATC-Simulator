d17f904b296e27648de8fc60193c346847164929
BIGJIANG <taylorjiang6168@gmail.com>
2025-03-30 18:07:41 +0800

    配置GitHub Pages自动部署

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    // 生成单个HTML文件
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        inlineDynamicImports: true
+      }
+    },
+    // 尝试将CSS内联到HTML中
+    cssCodeSplit: false
+  },
+  base: '/ATC-Simulator/'
 })
